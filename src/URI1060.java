d3b682461ba3f4d7da61d811b1b0a26bab323570
Henrique Matos <henriquemundimmatos@gmail.com>
2024-04-09 15:19:49 -0300

    feat: add URI 1060 solution

---
+++ src/URI1060.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class URI1060 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double num1, num2, num3, num4, num5, num6;
+        int positive = 0;
+
+        num1 = sc.nextDouble();
+        num2 = sc.nextDouble();
+        num3 = sc.nextDouble();
+        num4 = sc.nextDouble();
+        num5 = sc.nextDouble();
+        num6 = sc.nextDouble();
+
+        if(num1 > 0) {
+            positive++;
+        }
+        if(num2 > 0) {
+            positive++;
+        }
+        if(num3 > 0) {
+            positive++;
+        }
+        if(num4 > 0) {
+            positive++;
+        }
+        if(num5 > 0) {
+            positive++;
+        }
+        if(num6 > 0) {
+            positive++;
+        }
+
+        System.out.printf("%d valores positivos\n", positive);
+        sc.close();
+    }
+}
