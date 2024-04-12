e76f983d44c7ac591fc9f2e31535e369952d2e02
Henrique Matos <henriquemundimmatos@gmail.com>
2024-04-12 14:21:27 -0300

    feat: add array problem 01 solution

---
+++ src/exe01Array.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class exe01Array {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int position = 0;
+        int n = sc.nextInt();
+        double[] array = new double[n];
+        double bigger = Double.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextDouble();
+
+            if (bigger < array[i]) {
+                bigger = array[i];
+                position = i;
+            }
+        }
+
+        System.out.printf("%.1f\n%d", bigger, position);
+    }
+}
